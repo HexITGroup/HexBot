13655ad79fc6b1ad7ec882a31a4a8b05b17a5395
technicsparks <37851032+TechnicSparks@users.noreply.github.com>
2018-09-19 05:12:39 -0500

    updated to discord.py rewrite

--- hexbot/hexbot.py
+++ hexbot/hexbot.py
@@ -2,23 +2,26 @@
 #Bot created for the HexIT Discord server!
 
 import discord
+from discord.ext import commands 
 import json
 
-class Configuration:
+class Configuration:                                                           #Bot Token Handler
     def __init__(self, fileDir):
         f = open(fileDir, "r")
         jstr = f.read()
         jsn = json.loads(jstr)
         self.botToken = jsn["token"]
 
-class MyClient(discord.Client):
-    async def on_ready(self):
-        print('Logged on as {0}!'.format(self.user))
+bot = commands.Bot(command_prefix=".")
 
-    async def on_message(self, message):
-        print('Message from {0.author}: {0.content}'.format(message))
+@bot.event
+async def on_ready():
+    print("Logging in as: {0}".format(bot.user))
+
+@bot.event
+async def on_message(message):
+    print("In {0.guild} - #{0.channel}, {0.author} sent: {0.content}".format(message))
 
 #Initzalizing Client and Running Client
-clientConfig = Configuration("botcfg.json")
-client = MyClient()
-client.run(clientConfig.botToken)
+botConfig = Configuration("/var/botcfg.json")
+bot.run(botConfig.botToken)
